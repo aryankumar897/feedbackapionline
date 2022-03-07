c4b611d9ab9f49122ca882a1b690174bbda8145a
aryankumar <akumar07092000@gmail.com>
2022-03-07 15:33:37 +0530

    make it better

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -2,15 +2,13 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.emailFeedback = (req, res) => {
-    // console.log(req.body);
+    
     const { name, email, message, phone, uploadedFiles } = req.body;
     const emailData = {
         to: process.env.EMAIL_TO,
         from: email,
         subject: 'Feedback form',
         html: `
-
-       
             <h1>Customer Feedback Form</h1>
             <hr />
             <h2>Sender name: ${name}</h2>
@@ -18,27 +16,23 @@ exports.emailFeedback = (req, res) => {
             <h2>Sender message: ${message}</h2>
             <br />
             ${uploadedFiles.map(f => {
-                return 
-               ` <img src="${f.secure_url}" alt="${f.original_filename}" style="width:50%;overflow:hidden;padding:50px;"/>;`
-
-                
+                return `<img src="${f.secure_url}" alt="${f.original_filename}" style="width:50%;overflow:hidden;padding:50px;" />`;
             })}
             <hr />
             <p>https://feedbackonline.com</p>
-        
         `
     };
 
     sgMail
         .send(emailData)
         .then(sent => {
-          //  console.log(sent);
+           
             return res.json({
                 success: true
             });
         })
         .catch(err => {
-           // console.log(err);
+            
             return res.json({
                 success: false
             });
